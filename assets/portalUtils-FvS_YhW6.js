ce2b6a566fd9066267f206f6e092a9179d85a374
Willjfield <Willjfield@users.noreply.github.com>
2026-05-21 15:02:36 +0000

    deploy: b7a1e50f7fb61f13860d5c5654509a525637a4b5

---
+++ assets/portalUtils-FvS_YhW6.js
@@ -0,0 +1,1 @@
+import{M as u}from"./MapPage-DqZZuK33.js";function l(r,n){return r===null?n:new u({url:r.field("url")})}export{l};
---
+++ assets/queryZScale-DAn1LQTW.js
@@ -0,0 +1,1 @@
+import{a}from"./projectionUtils-BfJ7-_-l.js";function s(o,f,e){if(!e?.features||!e.hasZ)return;const t=a(e.geometryType,f,o.outSpatialReference);if(t!=null)for(const r of e.features)t(r.geometry)}export{s as t};
